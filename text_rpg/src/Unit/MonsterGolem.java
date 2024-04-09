6720967cb3144d8c669631d711d2cb868198d4da
iconew123 <iconew123@gmail.com>
2024-04-09 16:10:49 +0900

    Modify Class Name (Wolf > Golem)

---
+++ text_rpg/src/Unit/MonsterGolem.java
@@ -0,0 +1,9 @@
+package Unit;
+
+public class MonsterGolem extends Monster {
+
+	public MonsterGolem(String name, int hp, int power, int defense, int lv) {
+		super(name, hp, power, defense, lv);
+	}
+
+}
